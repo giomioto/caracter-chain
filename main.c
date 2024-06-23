1bc857f23d055bffd029b4c638842a3c370bcef4
Giovanni Mioto <89156343+giomioto@users.noreply.github.com>
2024-06-22 23:06:03 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 #define MAX_PALAVRAS 1000
 #define MAX_POSICOES 1000
 
-typedef struct {
+typedef struct
+{
     char palavra[100];
     int posicoes[MAX_POSICOES];
     int quantidade;
@@ -20,14 +21,17 @@ void AdicionarPalavra(const char *palavra, int posicao);
 void ArmazenarPalavras(const char linha[1000], int posicaoBase);
 void ImprimirPalavras();
 void ForcaBruta(char T[], long n, char P[], long m);
-void ConverterParaMinusculas(char *texto);
 void lerArquivo(const char *nomeArquivo, char *conteudo);
+void ProcurarPalavra(char *palavra, char *texto, int *posicaoBase);
+void imprimirPedaço(char *texto, int posicao);
 
-
-void AdicionarPalavra(const char *palavra, int posicao) {
-    for (int i = 0; i < numPalavras; i++) {
-        if (strcmp(palavras[i].palavra, palavra) == 0) { //se encontrar a palavra na posicao i
-            palavras[i].posicoes[palavras[i].quantidade] = posicao; //adiociona mais uma occorencia da palavra
+void AdicionarPalavra(const char *palavra, int posicao)
+{
+    for (int i = 0; i < numPalavras; i++)
+    {
+        if (strcmp(palavras[i].palavra, palavra) == 0)
+        {                                                           // se encontrar a palavra na posicao i
+            palavras[i].posicoes[palavras[i].quantidade] = posicao; // adiociona mais uma occorencia da palavra
             palavras[i].quantidade++;
             return;
         }
@@ -40,28 +44,34 @@ void AdicionarPalavra(const char *palavra, int posicao) {
     numPalavras++;
 }
 
-void ArmazenarPalavras(const char linha[1000], int posicaoBase) {
+void ArmazenarPalavras(const char linha[1000], int posicaoBase)
+{
     const char *delimitadores = " ,.?:!\n";
     char *copiaLinha = strdup(linha);
     char *palavra = strtok(copiaLinha, delimitadores);
 
     int posicao = posicaoBase;
-    while (palavra != NULL) {
+    while (palavra != NULL)
+    {
         // Armazenar a palavra apenas se não for um delimitador
-        if (strcmp(palavra, "-") != 0) {
+        if (strcmp(palavra, "-") != 0)
+        {
             AdicionarPalavra(palavra, posicao);
         }
-        posicao += strlen(palavra) + 1;  // +1 para pular o delimitador
+        posicao += strlen(palavra) + 1; // +1 para pular o delimitador
         palavra = strtok(NULL, delimitadores);
     }
 
     free(copiaLinha);
 }
 
-void ImprimirPalavras() {
-    for (int i = 0; i < numPalavras; i++) {
+void ImprimirPalavras()
+{
+    for (int i = 0; i < numPalavras; i++)
+    {
         printf("%s ", palavras[i].palavra);
-        for (int j = 0; j < palavras[i].quantidade; j++) {
+        for (int j = 0; j < palavras[i].quantidade; j++)
+        {
             printf("%d ", palavras[i].posicoes[j]);
         }
         printf("\n");
@@ -70,6 +80,8 @@ void ImprimirPalavras() {
 
 void ForcaBruta(char T[], long n, char P[], long m)
 {
+    int opcao = 0;
+    int cont = 0;
     long i, j, k;
     for (i = 1; i <= (n - m + 1); i++)
     {
@@ -81,21 +93,32 @@ void ForcaBruta(char T[], long n, char P[], long m)
             k++;
         }
         if (j > m)
-            printf("Casamento na posicao %3ld\n", i);
+        {
+            printf("Casamento na posição %3ld: ", i);
+
+            for (long l = i - 20; l <= i + 20; l++)
+            {
+                printf("%c", T[l]);
+            }
+            printf("\n");
+            cont++;
+        }
+    }
+
+    if (cont == 0)
+    {
+        printf("Padrão não encontrado\n");
+    }
+    else
+    {
+        printf("Padrão encontrado %d vezes\n", cont);
     }
 }
-
-void ConverterParaMinusculas(char *texto) {
-    for (int i = 0; texto[i]; i++) {
-        texto[i] = tolower(texto[i]);
-    }
-}
-
 
 void lerArquivo(const char *nomeArquivo, char *conteudo)
 {
     FILE *arquivo = fopen(nomeArquivo, "r");
-    char Linha[1000]; 
+    char Linha[1000];
     char *result;
 
     if (arquivo == NULL)
@@ -111,7 +134,6 @@ void lerArquivo(const char *nomeArquivo, char *conteudo)
     while ((result = fgets(Linha, 1000, arquivo)) != NULL)
     {
         strcat(conteudo, Linha);
-        ConverterParaMinusculas(Linha); // Converte a linha para minúsculas
         ArmazenarPalavras(Linha, posicaoBase);
         posicaoBase += strlen(Linha);
     }
@@ -119,26 +141,28 @@ void lerArquivo(const char *nomeArquivo, char *conteudo)
     quickSort(palavras, 0, numPalavras - 1);
 }
 
-void quickSort(Palavra *lista, int iniVet, int fimVet) {
-    if (iniVet < fimVet) {
+void quickSort(Palavra *lista, int iniVet, int fimVet)
+{
+    if (iniVet < fimVet)
+    {
         Palavra pivo = lista[fimVet];
         int i = iniVet - 1;
 
-        for (int j = iniVet; j < fimVet; j++) {
+        for (int j = iniVet; j < fimVet; j++)
+        {
 
-            if (strcmp(lista[j].palavra, pivo.palavra) < 0) {
+            if (strcmp(lista[j].palavra, pivo.palavra) < 0)
+            {
                 i++;
                 Palavra temp = lista[i];
                 lista[i] = lista[j];
                 lista[j] = temp;
-
             }
         }
 
         Palavra temp = lista[i + 1];
         lista[i + 1] = lista[fimVet];
         lista[fimVet] = temp;
-
 
         int pivoIndex = i + 1;
 
@@ -147,18 +171,68 @@ void quickSort(Palavra *lista, int iniVet, int fimVet) {
     }
 }
 
+void ProcurarPalavra(char *palavra, char *texto, int *posicaoBase)
+{
+    int ini = 0;
+    int fim = numPalavras - 1;
+    int meio;
+
+    while (ini <= fim)
+    {
+        meio = (ini + fim) / 2;
+
+        if (strcmp(palavras[meio].palavra, palavra) == 0)
+        {
+            if (*posicaoBase == 0)
+            {
+                printf("Ocorrências: %d \n", palavras[meio].quantidade);
+            }
+            printf("Palavra encontrada na posição: %d\n", palavras[meio].posicoes[*posicaoBase]);
+            if (*posicaoBase == palavras[meio].quantidade)
+            {
+                *posicaoBase = 0;
+            }
+            imprimirPedaço(texto, palavras[meio].posicoes[*posicaoBase]);
+            (*posicaoBase)++;
+            return;
+        }
+        else if (strcmp(palavras[meio].palavra, palavra) < 0)
+        {
+            ini = meio + 1;
+        }
+        else
+        {
+            fim = meio - 1;
+        }
+    }
+
+    printf("Palavra não encontrada\n");
+}
+
+void imprimirPedaço(char *texto, int posicao)
+{
+    for (int i = posicao - 20; i <= posicao + 20; i++)
+    {
+        printf("%c", texto[i]);
+    }
+    printf("\n");
+}
+
 int main()
 {
     int opcao;
     char texto[10000];
     char palavra[100];
+    int posicaoBase = 0;
 
     do
     {
         printf("\nSelecione uma opção:\n");
         printf("1. Abrir arquivo e exibir conteúdo\n");
-        printf("2. Procurar palavra no texto\n");
-        printf("3. Exibir palavras e posições armazenadas\n");
+        printf("2. Exibir palavras e posições armazenadas\n");
+        printf("3. Força Bruta\n");
+        printf("4. Procurar uma palavra no arquivo invertido\n");
+        printf("5. Procurar próxima ocorrencia\n");
         printf("0. Sair\n");
         printf("Opção: ");
         scanf("%d", &opcao);
@@ -171,13 +245,32 @@ int main()
             printf("Conteúdo do arquivo:\n%s\n", texto);
             break;
         case 2:
+            ImprimirPalavras();
+            break;
+        case 3:
             printf("Digite a palavra a ser buscada: ");
             fgets(palavra, 100, stdin);
             palavra[strcspn(palavra, "\n")] = '\0'; // Remove o '\n'
             ForcaBruta(texto, strlen(texto), palavra, strlen(palavra));
             break;
-        case 3:
-            ImprimirPalavras();
+        case 4:
+            posicaoBase = 0;
+            printf("Digite a palavra a ser buscada: ");
+            fgets(palavra, 100, stdin);
+            palavra[strcspn(palavra, "\n")] = '\0'; // Remove o '\n'
+            printf("\n\n----------------------------------------------\n");
+            ProcurarPalavra(palavra, texto, &posicaoBase);
+            printf("----------------------------------------------\n\n");
+            break;
+        case 5:
+            if (palavra[0] == '\0')
+            {
+                printf("Nenhuma palavra foi buscada\n");
+                break;
+            }
+            printf("\n\n----------------------------------------------\n");
+            ProcurarPalavra(palavra, texto, &posicaoBase);
+            printf("----------------------------------------------\n\n");
             break;
         case 0:
             printf("Saindo...\n");
